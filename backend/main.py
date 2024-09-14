fee0573bb68b9756861ae042c0d0ae5739531871
Manul Thanura <manulkarunarathna@gmail.com>
2024-09-14 12:10:01 +0530

    Create main.py

---
+++ backend/main.py
@@ -0,0 +1,36 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+from pydantic import BaseModel
+import requests
+
+from dotenv import load_dotenv
+import os
+
+load_dotenv()  # Load values from .env file
+
+PRIVATE_KEY = os.getenv('PRIVATE_KEY')
+
+app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+class User(BaseModel):
+    username: str
+
+@app.post('/authenticate')
+async def authenticate(user: User):
+    response = requests.put('https://api.chatengine.io/users/',
+        data={
+            "username": user.username,
+            "secret": user.username,
+            "first_name": user.username,
+        },
+        headers={ "Private-Key": PRIVATE_KEY }
+    )
+    return response.json()
